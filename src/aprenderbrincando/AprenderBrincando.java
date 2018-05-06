fba57d911154b271536be7f784769272bb5683e8
vinio2507 <vinio2507@gmail.com>
2018-05-06 19:41:06 -0400

    Voltando a Edição do projeto

--- src/aprenderbrincando/AprenderBrincando.java
+++ src/aprenderbrincando/AprenderBrincando.java
@@ -3,12 +3,11 @@ package aprenderbrincando;
 import aprenderbrincando.Controller.ControllerExecucao;
 import aprenderbrincando.Controller.TratadoraAction;
 import aprenderbrincando.View.Manipuladores;
-import aprenderbrincando.View.MenuPausa.MenuPausa;
 import aprenderbrincando.View.Splash;
 
 /**
  * Classe responsável por inicializar a aplicação
- * 
+ *
  * @author Vinícius Berto
  */
 public class AprenderBrincando {
@@ -18,17 +17,13 @@ public class AprenderBrincando {
     public static void main(String[] args) {
         ControllerExecucao ctrl = new ControllerExecucao();
         ctrl.setTa(new TratadoraAction(ctrl));
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                s = new Splash();
-
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            s = new Splash();
         });
         Manipuladores.aguardar(3000);
         s.setVisible(false);
         s = null;
         ctrl.setTa(new TratadoraAction(ctrl));
         ctrl.carregarInicio();
-       // Tips.exibirDica(ctrl.getTelaInicial(), ctrl.getTelaInicial().getCentro().getBtnIniciar());
     }
 }
